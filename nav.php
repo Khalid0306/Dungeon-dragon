fbbd118fd13e3bebc1d6b1406ce40b2fce07be14
Khalid Oumarou Garba <khalidogarba@gmail.com>
2023-04-12 08:54:12 +0200

    modif

--- nav.php
+++ nav.php
@@ -1,10 +1,13 @@
-<ul id="nav">
+<ul>
     <?php if (!isset($_SESSION['user'])) { ?>
         <li><a href="register.php">Créer un compte</a></li>
         <li><a href="login.php">Connexion</a></li>
     <?php } else { ?>
         <li><a href="persos.php"><?php echo $_SESSION['user']['email'] ?></a></li>
-        <li><a href="player_show.php">Mon compte</a></li>
-        <li><a href="logout.php">Logout</a></li>
+    <?php } ?>
+    <?php if (isset($_SESSION['user'])) { ?>
+
+        <li><a href="logout.php">Déconnexion</a></li>
+
     <?php } ?>
 </ul>
