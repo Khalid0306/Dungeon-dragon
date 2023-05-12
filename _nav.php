c37e1edbe6533f51103c56cf5a633a32645016ae
Khalid Oumarou Garba <khalidogarba@gmail.com>
2023-05-12 04:58:48 +0200

    Màj

--- _nav.php
+++ _nav.php
@@ -3,7 +3,7 @@
         <li><a href="register.php">Créer un compte</a></li>
         <li><a href="login.php">Connexion</a></li>
     <?php } else { ?>
-        <li><a href="persos.php"><?php echo $_SESSION['user']['email'] ?></a></li>
+        <li><a href="persos.php">Bastion</a></li>
         <li><a href="player_show.php">Mon compte</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php } ?>
